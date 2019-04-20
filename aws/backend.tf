745a90e989ab0e36e14b3cf1873259933db6fde4
vivek22117 <vivekmishra22117@gmail.com>
2019-04-20 21:23:22 +0530

    Added vpc module and jenkins module

--- aws/backend.tf
+++ aws/backend.tf
@@ -1,8 +1,32 @@
 resource "aws_s3_bucket" "main" {
-  bucket = "${var.s3_bucket_name}"
+  bucket = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
   acl    = "private"
-
-  tags = "${local.common_tags}"
-
+  tags   = "${local.common_tags}"
   region = "${var.default_region}"
+
+  lifecycle {
+    prevent_destroy = "true"
+  }
+
+  server_side_encryption_configuration {
+    "rule" {
+      "apply_server_side_encryption_by_default" {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    enabled = true
+    id      = "state"
+    prefix  = "state/"
+
+    noncurrent_version_expiration {
+      days = 1
+    }
+  }
 }
