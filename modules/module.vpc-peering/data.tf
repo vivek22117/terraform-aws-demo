e006b253f85cfa52856039909b5684e9bc49508c
vivek22117 <vivekmishra@22117>
2019-07-13 00:40:55 +0530

    Removed unwanted code

--- modules/module.vpc-peering/data.tf
+++ modules/module.vpc-peering/data.tf
@@ -42,14 +42,3 @@ data "terraform_remote_state" "vpc_shared" {
   }
 }
 
-provider "aws" {
-  region  = var.default_region
-  profile = var.profile
-
-  version = "2.17.0"
-}
-
-terraform {
-  required_version = ">= 0.12"
-}
-
